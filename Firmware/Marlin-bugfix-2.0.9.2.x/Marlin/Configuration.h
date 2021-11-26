0cc145859e07cb3c4c8cbd5c777d515f21381b38
Btrnq <b.tronquoy@gmail.com>
2021-11-26 22:00:58 +0100

    fix: MANUAL_X_HOME_POS to center bed

--- Firmware/Marlin-bugfix-2.0.9.2.x/Marlin/Configuration.h
+++ Firmware/Marlin-bugfix-2.0.9.2.x/Marlin/Configuration.h
@@ -1731,7 +1731,7 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS 0
+#define MANUAL_X_HOME_POS -24
 //#define MANUAL_Y_HOME_POS 0
 //#define MANUAL_Z_HOME_POS 0
 //#define MANUAL_I_HOME_POS 0
